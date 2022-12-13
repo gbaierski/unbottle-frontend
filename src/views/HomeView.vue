75b7f33f50121708bd12633dd9445e89b54d10ad
Gustavo Baierski <gustavobaierski@gmail.com>
2022-12-13 02:31:31 -0300

    feat: Aprimorando primeira parte da home e iniciando segunda

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,10 +17,40 @@
     </div>
   </header>
   <section class="container image medium" id="home-1">
+    <div id="home-1-content">
+      <div id="home-1-messages">
+        <p id="home-1-first-message" class="text-home">Seja bem-vindo!</p>
+        <p id="home-1-second-message" class="text-home">Estamos transbordando de alegria por ter você por aqui :D</p>
+      </div>
+      <div id="home-1-scroll">
+        <p id="home-1-third-message" class="text-home">Vamos começar!</p>
+        <img src="../assets/images/icons/downArrow.png" alt="DownArrow" id="home-1-first-image">
+      </div>
+  </div>
   </section>
   <section class="container image big" id="home-2">
+    <div id="home-2-content">
+      <div id="home-2-content-div-1">
+        <p id="home-2-first-message" class="text-home">Temos várias cores!</p>
+      </div>
+      <div id="home-2-content-div-2">
+        <p id="home-2-second-message" class="text-home">Inclusive a sua favorita :)</p>
+      </div>
+      <div id="home-2-content-div-3">
+        <div id="products-list">
+          <p>Confira o nosso catálogo:</p>
+          <button>A</button>
+        </div>
+      </div>
+    </div>
   </section>
   <section class="container image big" id="home-3">
+  </section>
+  <section class="container small" id="home-4">
+  </section>
+  <section class="container image medium" id="home-5">
+  </section>
+  <section class="container small" id="home-6">
   </section>
 </template>
 <style scoped>
@@ -28,20 +58,25 @@
 #home-1 { background-image: url("../assets/images/wallpapers/main.jpg"); }
 #home-2 { background-image: url("../assets/images/wallpapers/home.jpg"); }
 #home-3 { background-image: url("../assets/images/wallpapers/custom.jpg"); }
+#home-5 { background-image: url("../assets/images/wallpapers/save-planet.jpg"); }
 
 header {
   background-color: white;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
   width: 100%;
   position: fixed;
   display: flex;
   justify-content: space-around;
   align-items: center;
+  z-index: 999;
 }
 
 a {
   text-decoration: none;
   color: gray
 }
+
+/* --- HEADER --- */
 
 #logo-header {
   width: 100px;
@@ -117,6 +152,76 @@ a {
 #searchbutton-header:active {
   transform: scale(0.8);
 }
+
+/* --- HOME 1 --- */
+
+#home-1-content {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+#home-1-messages {
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  height: 50%;
+}
+#home-1-scroll {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 15%;
+  padding-top: 35%;
+}
+#home-1-first-message {
+  font-size: 120px;
+  margin: 0px;
+}
+#home-1-second-message {
+  font-size: 30px;
+  margin: 25px;
+}
+#home-1-third-message {
+  font-size: 25px;
+}
+#home-1-first-image {
+  width: 70px;
+  margin-top: -20px;
+}
+
+/* --- HOME 2 --- */
+
+#home-2-content {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+}
+#home-2-content-div-1 {
+  width: 100%;
+  height: 15%;
+  padding-top: 15%;
+  padding-right: 5%;
+  display: flex;
+  justify-content: end;
+}
+#home-2-first-message {
+  transform: rotate(40deg);
+  font-size: 80px;
+  margin: 0px;
+}
+#home-2-content-div-2 {
+  width: 97.5%;
+  height: 10%;
+  padding-top: 20%;
+  display: flex;
+}
+#home-2-second-message {
+  transform: rotate(-35deg);
+  font-size: 70px;
+  margin: 0px;
+}
 .big { height: 200vh; }
 .medium { height: 110vh }
 .small { height: 60vh; }
@@ -134,5 +239,12 @@ a {
   cursor: pointer;
   color: #add8e6;
   transform: scale(1.05);
+}
+.text-home {
+  color: white;
+  text-shadow: 2px 4px 3px rgba(0,0,0,0.8);
+}
+.container {
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
 }
 </style>
